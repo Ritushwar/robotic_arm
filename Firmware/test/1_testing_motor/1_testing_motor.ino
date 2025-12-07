1a5addfe1a7ed67ebf4a9203f607bf50de28adda
Ritushwar <ritushwarneupane111@gmail.com>
2025-12-07 22:13:37 +0545

    Testing single motor

---
+++ Firmware/test/1_testing_motor/1_testing_motor.ino
@@ -0,0 +1,15 @@
+#include <AccelStepper.h>
+#define EN_PIN 8
+AccelStepper stepper(AccelStepper::DRIVER, 2, 5);
+
+void setup() {
+  stepper.setMaxSpeed(1000);
+  stepper.setAcceleration(400);
+  pinMode(EN_PIN, OUTPUT);
+  digitalWrite(EN_PIN, LOW);
+}
+
+void loop() {
+  stepper.setSpeed(600);
+  stepper.runSpeed();
+}
