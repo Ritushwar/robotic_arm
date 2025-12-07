72e53ca901ec457832dd392c172b556aba32e0e2
Ritushwar <ritushwarneupane111@gmail.com>
2025-12-07 22:19:05 +0545

    Read the potentiometer for feedback

---
+++ Firmware/test/5_potentiometer_read/5_potentiometer_read.ino
@@ -0,0 +1,16 @@
+int pos_x = A0;
+int value = 0;
+int angle = 0;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pos_x, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  value = analogRead(pos_x);
+  angle = map(value,0,1023,0,180);
+  Serial.println(angle);
+  delay(1000);
+}
