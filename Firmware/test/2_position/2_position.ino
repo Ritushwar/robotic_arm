395b5403b56eafdbccc07cd72b0fd53757b23bf2
Ritushwar <ritushwarneupane111@gmail.com>
2026-01-02 00:46:03 +0545

    take input from user

--- Firmware/test/2_position/2_position.ino
+++ Firmware/test/2_position/2_position.ino
@@ -1,23 +1,35 @@
 #include <AccelStepper.h>
 #define EN_PIN 8
+int steps;
 AccelStepper stepper(AccelStepper::DRIVER, 2, 5);
 int steps_per_degrees = 1;
 int target_degrees = 90;
 int target_steps = steps_per_degrees * target_degrees;
 
 void setup() {
-  stepper.setMaxSpeed(1000);
-  stepper.setAcceleration(300);
+  Serial.begin(9600);
+  stepper.setMaxSpeed(900);
+  stepper.setAcceleration(500);
   pinMode(EN_PIN, OUTPUT);
   digitalWrite(EN_PIN, LOW);
 }
 
 void loop() {
-  stepper.moveTo(200);
-  stepper.runToPosition();
-  delay(1000);
-
-  stepper.moveTo(0);
-  stepper.runToPosition();
-  delay(1000);
+  if (Serial.available() > 0){
+    String input = Serial.readStringUntil('\n');
+    steps = input.toInt();
+    Serial.println(steps);
+    if(steps < 0){
+      stepper.moveTo(steps);
+      stepper.runToPosition();
+      delay(1000);
+      steps = 0;
+    }
+    else{
+      stepper.moveTo(steps);
+      stepper.runToPosition();
+      delay(1000);
+      steps = 0;
+    }
+  }
 }
